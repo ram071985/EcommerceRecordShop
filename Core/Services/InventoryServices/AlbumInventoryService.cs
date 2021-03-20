dce3c77ffae6cc75762812834f2af51760102e52
ram071985 <reidmuchow@gmail.com>
2021-03-19 22:13:20 -0500

    added get method to albumInventoryService.

--- Core/Services/InventoryServices/AlbumInventoryService.cs
+++ Core/Services/InventoryServices/AlbumInventoryService.cs
@@ -1,10 +1,33 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Core.Entities;
+
 namespace Core.Services.InventoryServices
 {
-    public class AlbumInventoryService
+
+    public interface IAlbumInventoryService
     {
-        public AlbumInventoryService()
+
+    }
+
+    public class AlbumInventoryService : IAlbumInventoryService
+    {
+        public List<Album> Get()
         {
+            var albums = new List<Album>();
+
+            var album = new Album
+            {
+                Id = 1,
+                ArtistName = "Stevie Wonder",
+                AlbumName = "Innervisions",
+                Price = 15.00M
+            };
+            albums.Add(album);
+
+            return albums;
         }
+            
     }
 }
