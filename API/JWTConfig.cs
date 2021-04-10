53e07b4a91c2b5a9fb61c455b374ed5899e39139
ram071985 <reidmuchow@gmail.com>
2021-04-10 13:27:39 -0500

    getting back to speed

---
+++ API/JWTConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using Core.Services.AuthenticationServices;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API
+{
+    public static class JwtConfig
+    {
+        public static void Configure(IServiceCollection services, IConfiguration configuration)
+        {
+            var key = configuration["JwtKey"];
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            // TODO will need to pass in an instance of a database authentication class 
+            services.AddSingleton<IGenerateJwtToken>(
+                new GenerateJwtToken(key));
+        }
+    }
+}
