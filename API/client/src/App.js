f31748c6d60e8701df90bb607fc78b5633f74367
Aliel Reyes <alielreyes@gmail.com>
2021-04-10 14:05:08 -0700

    Added react router. The navbar logic is set to work with react router links using the useHistroy hook

--- API/client/src/App.js
+++ API/client/src/App.js
@@ -1,10 +1,24 @@
+import { Route, Switch } from "react-router";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Home from "./components/Pages/Home/Home";
+import Shop from "./components/Pages/Shop/Shop";
+import Product from "./components/Pages/Product/Product";
+import Payment from "./components/Pages/Payment/Payment";
+import TransactionHistory from "./components/Pages/TransactionHistory/TransactionHistory";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Route path="/shop" component={Shop} />
+        <Route path="/product" component={Product} />
+        <Route path="/payment" component={Payment} />
+        <Route path="/transaction-history" component={TransactionHistory} />
+        <Route path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
---
+++ API/client/src/components/Pages/TransactionHistory/TransactionHistory.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TransactionHistory = () => {
+  return <div>Transaction History Page</div>;
+};
+
+export default TransactionHistory;
