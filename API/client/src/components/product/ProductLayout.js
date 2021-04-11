41caaed4b4521e82f8e90a5b3078ad69b642b328
ram071985 <reidmuchow@gmail.com>
2021-04-10 19:33:08 -0500

    added project components, api request to spotify, config settings, and populated product page with spotify album data.

--- API/client/src/components/product/ProductLayout.js
+++ API/client/src/components/product/ProductLayout.js
@@ -1,14 +1,49 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import axios from "axios";
 
 const ProductLayout = () => {
+  const [artistName, setArtistName] = useState("Kiefer");
+  const [albumName, setAlbumName] = useState("Happysad");
+  const [image, setImage] = useState(
+    "https://i.scdn.co/image/ab67616d0000b2738396e7dd8de7725b5894a36d"
+  );
+  //   useEffect(() => {
+  //     getData();
+  //   });
+
+  const getData = () => {
+    const id = "7t0S1JQpSHsZftfzVQWW2a";
+    axios
+      .get(`spotify/${id}`)
+      .then((response) => {
+        setArtistName(response.data.artistName);
+        setImage(response.data.imageUrl);
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <Container fluid>
-        <Row>
-          <Col className="w-75">How</Col>
-          <Col md={4}>How</Col>
-        </Row>
-   </Container>
+      <Row>
+        <Col className="left-col">
+          <img className="img-fluid" src={image} />
+        </Col>
+        <Col md={4}>
+          <span className="product-titles" id="artist-name">
+            {artistName}
+          </span>
+          <span className="product-titles" id="album-name">
+            {albumName}
+          </span>
+          <span className="product-titles mt-3" id="price">
+            $20
+          </span>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
