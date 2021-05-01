fe97f730a71a2a88c1bbb15e83517ff0da4c52c9
ram071985 <reidmuchow@gmail.com>
2021-04-30 19:50:03 -0500

    added view tracks drop down

--- API/client/src/components/product/ProductLayout.js
+++ API/client/src/components/product/ProductLayout.js
@@ -3,50 +3,60 @@ import { Container, Row, Col, Link } from "react-bootstrap";
 import axios from "axios";
 import { CIcon } from "@coreui/icons-react";
 import { freeSet } from "@coreui/icons";
-import { CPopover, CLink } from "@coreui/react";
+import {
+  CPopover,
+  CLink,
+  CDropdown,
+  CDropdownDivider,
+  CDropdownHeader,
+  CDropdownItem,
+  CDropdownMenu,
+  CDropdownToggle,
+  CDataTable,
+} from "@coreui/react";
 
 const ProductLayout = () => {
   const [artistName, setArtistName] = useState("Kiefer");
   const [albumName, setAlbumName] = useState(" Happysad");
+  const [albumResults, setAlbumResults] = useState([]);
   const [image, setImage] = useState(
     "https://i.scdn.co/image/ab67616d0000b2738396e7dd8de7725b5894a36d"
   );
-  //   useEffect(() => {
-  //     getData();
-  //   });
+  const fields = ["Name", "Duration"];
 
-  const getData = () => {
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
     const id = "7t0S1JQpSHsZftfzVQWW2a";
-    axios
-      .get(`spotify/${id}`)
-      .then((response) => {
-        setArtistName(response.data.artistName);
-        setImage(response.data.imageUrl);
-        console.log(response);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    const response = await axios.get(`spotify/${id}`);
+    setAlbumResults(response.data);
   };
+
+  const renderTracks = () => {
+    getData();
+    albumResults.tracks.map((track, index) => track.name);
+  };
+  console.log(albumResults);
   return (
     <div className="main-container d-flex">
-      <div classname="left-container container-fluid">
+      <div className="left-container container-fluid">
         <img id="album-img" className="img-fluid" src={image} />
       </div>
       <div className="right-container">
-        {" "}
         <div className="pname-container container-fluid d-flex flex-wrap">
           <h1 className="product-details mb-0" id="artist-name">
             Kiefer
           </h1>
           <div className="favorite-container">
-          <span className="icon ml-5">
-            <CPopover placement="bottom" className="favorites-popover" content="Add To Wishlist">
-              <CLink >
-                <CIcon className="ml-1" content={freeSet.cilBookmark} />
-              </CLink>
-            </CPopover>
-          </span>
+            <span className="icon ml-5">
+              <CPopover placement="bottom" content="Add To Wishlist">
+                <CLink className="wishlist-link">
+                  <CIcon className="ml-1" content={freeSet.cilBookmark} />
+                </CLink>
+              </CPopover>
+            </span>
           </div>
         </div>
         <h1 className="product-details d-inline mr-5" id="album-name">
@@ -55,6 +65,26 @@ const ProductLayout = () => {
         <div className="price-container">
           <h3 id="price">$20</h3>
         </div>
+        <CDropdown>
+          <CDropdownToggle color="secondary">View tracks</CDropdownToggle>
+          <CDropdownMenu className="tracks-dropdown">
+            <table>
+              <tr>
+                <th>Name</th>
+                <th colspan="2">Duration</th>
+              </tr>
+              {albumResults.tracks &&
+                albumResults.tracks.map((item, index) => (
+                  <div>
+                    <tr>
+                      <td key={index}>{item.name}</td>
+                      <td key={index}>{item.duration}</td>
+                    </tr>
+                  </div>
+                ))}
+            </table>
+          </CDropdownMenu>
+        </CDropdown>
       </div>
     </div>
   );
