580506f9ab14f9ada8f2dfd446e1dc7891f56325
bbastanza <b.bastanza@gmail.com>
2021-05-06 16:13:21 -0700

    fix merge confilct in Order.cs

--- Core/Entities/Order.cs
+++ Core/Entities/Order.cs
@@ -10,18 +10,6 @@ namespace Core.Entities
         [Required]
         [Column(TypeName = "varchar(50)")]
         public string Id { get; set; } = Guid.NewGuid().ToString();
-<<<<<<< HEAD
-
-        [Required]
-        [Column(TypeName = "varchar(50)")]
-        public string UserId { get; set; }
-
-        [Required] 
-        public DateTime PurchaseDate { get; set; } = DateTime.Now;
-
-        [Required] 
-        public DateTime ShippingDate { get; set; } = DateTime.Now.AddDays(3);
-=======
         
         [Required]
         [Column(TypeName = "varchar(50)")]
@@ -38,10 +26,6 @@ namespace Core.Entities
         
         [Required]
         public List<Product> Products { get; set; }
->>>>>>> f1981c69922911dddca4f3da6f78c7fc9e311e2f
-
-        [Required] 
-        public DateTime CanReturnBy { get; set; } = DateTime.Now.AddDays(30);
 
         [Required]
         public decimal TotalPrice
@@ -53,7 +37,5 @@ namespace Core.Entities
                 return totalPrice;
             }
         }
-        [Required] 
-        public List<Product> Products { get; set; }
     }
 }
