a9c01fc791f5760f3bb172203c4223470ea3144d
Aliel Reyes <alielreyes@gmail.com>
2021-05-13 23:18:56 -0700

    Added custom controls. Added breakpoint logic. Moved Button component to UI folder componet since it is a pure UI component. Included new variants for carousel and Button UI components

--- API/client/src/assets/icons/index.js
+++ API/client/src/assets/icons/index.js
@@ -1,9 +1,16 @@
-import { cilUser, cilMagnifyingGlass } from "@coreui/icons";
+import {
+  cilUser,
+  cilMagnifyingGlass,
+  cilArrowThickLeft,
+  cilArrowThickRight,
+} from "@coreui/icons";
 
 export const icons = Object.assign(
   {},
   {
     cilUser,
     cilMagnifyingGlass,
+    cilArrowThickLeft,
+    cilArrowThickRight,
   }
 );
--- API/client/src/components/Button/Button.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Button.scss";
-
-const Button = (props) => {
-  return <button className={"Button"}>Button</button>;
-};
-
-export default Button;
