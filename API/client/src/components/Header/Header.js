336bea9729d0c2b46689c54231cafef73ccb69d0
Aliel Reyes <alielreyes@gmail.com>
2021-05-25 07:31:58 -0700

    Header is now transparent only on the home page. Had to adjust other UI elements in the process in order to make this work. When the searchbar opens up if you click anywhere it will now go away. When using the searchbar or the side drawer menu, the navbar will become solid. If the mouse hovers over the navbar it will also become solid using a transition. Loading Spinner is now centered a bit more when loading the slides. Adjusted the positioning of the details carousel so that it works with the new header. Removed console.log statements on the products fetch api call.

--- API/client/src/components/Header/Header.js
+++ API/client/src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import { CNavbar, CToggler } from "@coreui/react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import "./Header.scss";
 
 import NavbarLinks from "./NavbarLinks/NavbarLinks";
@@ -10,10 +11,20 @@ import Searchbar from "./Searchbar/Searchbar";
 // TODO: Remove shop from navbar when the drawer is open. Copy the logic similar to Collapsible Navbar.
 
 const Header = (props) => {
+  let location = useLocation();
   const [showDrawer, setShowDrawer] = useState(false);
   const [showSearchbar, setShowSearchbar] = useState(false);
 
   const [loggedIn] = useState(true);
+  const [headerIsSolid, setHeaderIsSolid] = useState(true);
+
+  useEffect(() => {
+    if (location.pathname === "/home" || location.pathname === "/") {
+      setHeaderIsSolid(false);
+    } else {
+      setHeaderIsSolid(true);
+    }
+  }, [location.pathname]);
 
   const drawerLinks = [
     {
@@ -34,10 +45,17 @@ const Header = (props) => {
     setShowDrawer(newValue);
   };
 
+  const isNavbarSolid = () => showSearchbar || showDrawer || headerIsSolid;
+
   return (
-    <header>
+    <header style={{ position: `${headerIsSolid ? "relative" : "absolute"}` }}>
       <div className="headerContent">
-        <CNavbar className="customNavbar" sticky light expandable="sm">
+        <CNavbar
+          className={`customNavbar ${isNavbarSolid() ? "solid" : ""}`}
+          sticky
+          light
+          expandable="sm"
+        >
           {loggedIn ? <CToggler inNavbar onClick={toggleDrawer} /> : null}
           <NavbarLinks
             loggedIn={loggedIn}
@@ -47,7 +65,12 @@ const Header = (props) => {
           />
         </CNavbar>
 
-        {loggedIn ? <Searchbar active={showSearchbar} /> : null}
+        {loggedIn ? (
+          <Searchbar
+            active={showSearchbar}
+            close={() => setShowSearchbar(false)}
+          />
+        ) : null}
 
         <DrawerModal isOpen={showDrawer} close={() => setShowDrawer(false)}>
           <DrawerMenu links={drawerLinks} />
--- API/client/src/services/api/products.js
+++ API/client/src/services/api/products.js
@@ -3,10 +3,9 @@ import axios from "../../config/api/index";
 const fetchProducts = async (count) => {
   try {
     const res = await axios.get(`/products/${count}`);
-    console.log(res);
     if (res.status !== 200) {
       const data = res.data;
-      throw new Error("Something went wrong");
+      throw new Error("Fetch Products API call failed");
     }
 
     return res.data;
