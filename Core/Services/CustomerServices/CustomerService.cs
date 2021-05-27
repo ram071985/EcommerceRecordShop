975e63e3a5e700fe2ed095521a7afeb764137e15
bbastanza <b.bastanza@gmail.com>
2021-05-26 18:26:39 -0700

    add customer activate and deactivate methods

--- Core/Services/CustomerServices/CustomerService.cs
+++ Core/Services/CustomerServices/CustomerService.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using Core.DataAccess;
 using Core.Entities;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Bcrypt = BCrypt.Net.BCrypt;
 
 namespace Core.Services.CustomerServices
 {
@@ -11,6 +13,8 @@ namespace Core.Services.CustomerServices
     {
         Customer GetCustomerById(string customerId);
         void AddCustomer(string customerName, string password, string email);
+        void DeactivateCustomer(string customerId);
+        void ActivateCustomer(string customerId);
     }
 
     public class CustomerService : ICustomerService
@@ -23,18 +27,23 @@ namespace Core.Services.CustomerServices
             _hardSalt = configuration["PasswordSalt"];
             _db = db;
         }
-        
+
         public Customer GetCustomerById(string customerId)
         {
-            var customer = _db.Customers.FirstOrDefault(x => x.Id == customerId); 
-            
+            var customer = _db.Customers.FirstOrDefault(x => x.Id == customerId);
+
             return customer;
         }
 
         public void AddCustomer(string customerName, string password, string email)
         {
+            var dbCustomer = _db.Customers.FirstOrDefault(x => x.CustomerName == customerName);
+
+            if (dbCustomer != null)
+                throw new Exception("already a customer with this customername");
+
             var hashedPassword = HashPassword(password);
-            
+
             var customer = new Customer
             {
                 Id = Guid.NewGuid().ToString(),
@@ -44,9 +53,45 @@ namespace Core.Services.CustomerServices
                 WalletBalance = 3000,
                 CreatedAt = DateTime.Now,
                 Orders = new List<Order>(),
+                IsActive = true
             };
 
             _db.Add(customer);
+            _db.SaveChanges();
+        }
+
+        public void DeactivateCustomer(string customerId)
+        {
+            var customer = _db.Customers.Where(x => x.Id == customerId)
+                .Include(x => x.CartItems)
+                .FirstOrDefault();
+
+            if (customer == null)
+                throw new Exception("customer with this id is null");
+
+            if (customer.IsActive) return;
+
+            customer.IsActive = false;
+
+            if (customer.CartItems is {Count: > 0})
+                _db.RemoveRange(customer.CartItems);
+
+            _db.Update(customer);
+            _db.SaveChanges();
+        }
+
+        public void ActivateCustomer(string customerId)
+        {
+            var customer = _db.Customers.FirstOrDefault(x => x.Id == customerId);
+
+            if (customer == null)
+                throw new Exception("customer with this id is null");
+
+            if (!customer.IsActive) return;
+
+            customer.IsActive = true;
+
+            _db.Update(customer);
             _db.SaveChanges();
         }
 
@@ -54,7 +99,7 @@ namespace Core.Services.CustomerServices
         {
             var passwordToHash = password + _hardSalt;
             var passwordSalt = BCrypt.Net.BCrypt.GenerateSalt();
-            return BCrypt.Net.BCrypt.HashPassword(passwordToHash, passwordSalt);
+            return Bcrypt.HashPassword(passwordToHash, passwordSalt);
         }
     }
 }
